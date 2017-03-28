9d8ba05d7a2d19093e3cf5fef6cfbccbd6f35461
Jefeson Alves <jefesonbezerra@gmail.com>
2017-03-28 00:53:42 -0300

    Update backupFortigate.py
    Funcionando...

--- backupFortigate.py
+++ backupFortigate.py
@@ -3,20 +3,32 @@
 # Desenvolvido por Jefeson Ales
 # e-mail: jefesonbezerra@gmail.com
 #
-# ----Em desenvolvimento----
-# 
 # Requisitos:
 # - Cliente SCP
 # - Usuário READ-ONLY no Fortigate
 # - SCP Habilitado no Fortigate com o seguinte comando
+# Fonte: http://kb.fortinet.com/kb/documentLink.do?externalID=12002
 
-import os
+import os 
+import time
+from datetime import datetime
 
-nomefwl = input ("Digite o nome ou localidade do Firewall: ")
-ipfwl = input ("Digite o ip do Firewall: ")
-porta = input ("Digite a porta ssh do Firewall: ")
-usuario = input ("Digite o usuário do Firewall: ")
-caminho = input ("Digite o local para armazenar o backup: ")
-datahora = input ("Digite a data do backup (formato: ddmmaaaahhmm ")
+os.system("clear")
+print ("------------------------------------")
+print ("Script para backup do Fortigate.")
+print ("Aguarde tres segundos...")
+print ("------------------------------------")
+time.sleep(3)
 
-os.system("scp " + "-p " + porta + " " + usuario + "@" + ipfwl + ":fgt-config:" + caminho + nomefwl + datahora + ".conf")
+print ("----------------------------------------------")
+print ("Preencha as informações relativas ao firewall:")
+print ("----------------------------------------------")
+siglalocal = input ("1. Digite a sigla da localidade: ").upper()
+ipfwl = input ("2. Digite o endereço IP: ")
+porta = input ("3. Digite a porta scp ")
+usuario = input ("4. Digite o usuário: ")
+caminho = input ("5. Digite o local para armazenar o backup: ")
+
+data = datetime.now().strftime('%d%m%Y_%H%M')
+
+os.system("scp -P " + porta + " " + usuario + "@" + ipfwl + ":sys_config " + caminho + "FGT" + siglalocal + data + ".conf")
