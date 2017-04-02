f2ca80bcd8ee1edc9537417c59591c29b51c5fc5
Jefeson Alves <jefesonbezerra@gmail.com>
2017-04-02 19:54:14 -0300

    Update backupFortigate.py
    Adicionado os comentários...

--- backupFortigate.py
+++ backupFortigate.py
@@ -1,35 +1,35 @@
 # This Python file uses the following encoding: utf-8
-# Script para reaalização de backup do Firewall Fortigate da Fortinet
-# Desenvolvido por Jefeson Ales
-# e-mail: jefesonbezerra@gmail.com
-#
-# Requisitos:
-# - Cliente SCP
-# - Usuário READ-ONLY no Fortigate
-# - SCP Habilitado no Fortigate com o seguinte comando
-# Fonte: http://kb.fortinet.com/kb/documentLink.do?externalID=12002
+'''
+Script para realização de backup do Firewall Fortigate da Fortinet
+Desenvolvido por Jefeson Ales
+e-mail: jefesonbezerra@gmail.com
+Requisitos:
+- Sistema operacional Linux;
+- Cliente SCP;
+- Usuário READ-ONLY no Fortigate;
+- SCP Habilitado no Fortigate;
 
+Fonte: http://kb.fortinet.com/kb/documentLink.do?externalID=12002
+'''
+
+# Importando os módulo
 import os 
 import time
 from datetime import datetime
 
-os.system("clear")
-os.system("mkdir ~/backup_fgt/")
-print ("------------------------------------")
-print ("Script para backup do Fortigate.")
-print ("Aguarde tres segundos...")
-print ("------------------------------------")
-time.sleep(3)
-
+os.system("clear") # Limpar a tela 
+os.system("mkdir ~/backup_fgt/") # Criação do diretório backup_fgt
 print ("----------------------------------------------")
 print ("Preencha as informações relativas ao firewall:")
 print ("----------------------------------------------")
-siglalocal = input ("1. Digite a sigla da localidade: ").upper()
-ipfwl = input ("2. Digite o endereço IP: ")
-porta = input ("3. Digite a porta scp ")
-usuario = input ("4. Digite o usuário: ")
-caminho = ("~/backup_fgt/")
 
-data = datetime.now().strftime('%d%m%Y_%H%M')
+# Declaração de variáveis
+siglalocal = input ("1. Digite a sigla da localidade: ").upper() #Variável que armazena a localidade
+ipfwl = input ("2. Digite o endereço IP: ") # Variável que armazena o endereço IP
+porta = input ("3. Digite a porta scp: ") # Variável que armazena a porta do ssh
+usuario = input ("4. Digite o usuário: ") # Variável que armazena a conta do usuário do fortigate
+caminho = ("~/backup_fgt/") # Variável que armazena o caminho onde será salvo o backup
+data = datetime.now().strftime('%d%m%Y_%H%M') # Variável que armazena o dia, mês, ano e horário atual
 
-os.system("scp -P " + porta + " " + usuario + "@" + ipfwl + ":sys_config " + caminho + "FGT-" + siglalocal + data + ".conf")
+# Comando do linux para executar o backup utilizando as varíáveis declaradas
+os.system("scp -P " + porta + " " + usuario + "@" + ipfwl + ":sys_config " + caminho + "FGT-" + siglalocal + data + ".conf") 
