7dcd4ac8e9b4bdbb407018536dde0502c2c60fef
jefesonalves <jefesonbezerra@gmail.com>
2024-05-19 17:44:49 -0300

    utilizando funções

--- scripts/ren_varios_arquivos.py
+++ scripts/ren_varios_arquivos.py
@@ -1,19 +1,59 @@
 import os
 
-dir = input("Digite o diretório completo:")
-os.chdir(dir)
-files = (os.listdir(dir))
-limit = int(len(files))
-prefix = input("Digite o prefixo:")
-ext = "."+input("Digite a extensão do arquivo:")
-os.system("clear")
+def verificar_dir():
+    dir_atual = os.getcwd()
+    return dir_atual
 
-for file in range(limit):
-    print("# Processo:",file + 1)   
-    old_file = files[file]
-    print("Nome do arquivo original: "+old_file)
-    new_file = prefix+str(file)+ext
-    os.rename(old_file, new_file)
-    print("Arquivo renomeado para: "+new_file)
-    print("\n")
-print("Arquivos renomeados!")
+def mudar_dir():
+    dir_informado = os.chdir(input("Informe:"))
+    return dir_informado
+
+def contar_arquivos():
+    quant_arquivos = int(len(os.listdir()))
+    return quant_arquivos
+
+def listar_arquivos():
+    arquivos = (os.listdir())
+    return arquivos
+
+def definir_name(prefixo):
+    for i in range(0, 10, 1):
+        prefixo = prefixo+f'{i:04d}'
+        return prefixo
+
+mudar_dir()
+
+
+# def prefixo_ext():
+#     for i in range(0, 1001, 1):
+#         prefix = input("Informe o prefixo: "+ f'MOVI{i:04d}')
+#         ext = input("Informe a extensão: ")
+#         print(prefix+ext)
+        
+#         print("# Processo:",file + 1)   
+#         old_file = files[file]
+#         print("Nome do arquivo original: "+old_file)
+#         new_file = prefix+str(file)+ext
+#         os.rename(old_file, new_file)
+#         print("Arquivo renomeado para: "+new_file)
+#         print("\n")
+#         print("Arquivos renomeados!")
+
+
+# diretorio()
+# prefixo_ext()
+
+ 
+# 
+# 
+# os.system("clear")
+
+# for file in range(limit):
+#     print("# Processo:",file + 1)   
+#     old_file = files[file]
+#     print("Nome do arquivo original: "+old_file)
+#     new_file = prefix+str(file)+ext
+#     os.rename(old_file, new_file)
+#     print("Arquivo renomeado para: "+new_file)
+#     print("\n")
+# print("Arquivos renomeados!")
