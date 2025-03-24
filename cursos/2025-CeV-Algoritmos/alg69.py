2e273c53f4eba76b08f237f5a5d23a2ebbe2b6dd
Jefeson <jefesonbezerra@gmail.com>
2025-03-24 18:13:23 -0300

    Correção do alg69

--- cursos/2025-CeV-Algoritmos/alg69.py
+++ cursos/2025-CeV-Algoritmos/alg69.py
@@ -1,12 +1,10 @@
 a1 = int(input("Digite o primeiro termo da PA: "))
 r = int(input("Digite a razão da PA: "))
-qtd_elementos = 10
+lista = []
 
-for i in range(1, qtd_elementos):
-# pa = [a1 + i * r for i in range(10)]
-    pa = a1 + (qtd_elementos - 1) * r
+for i in range(0, 10):
+    pa = a1 + i * r
+    lista.append(pa)
 
-# total = sum(pa)
-
-print("Os 10 primeiros termos da PA são:", pa)
-# print("A soma dos 10 primeiros termos é:", total)
+print(f"Os 10 primeiros termos da PA são: {lista}")
+print(f"A soma dos 10 primeiros termos é: {sum(lista)}")
